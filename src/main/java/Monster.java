e0051dc8dea90b461067565764b8435afdc73ece
diogotvf7 <up202108752@g.uporto.pt>
2022-10-10 01:15:47 +0100

    Only added Super Deadly Enemies now. Oopsie

---
+++ src/main/java/Monster.java
@@ -0,0 +1,31 @@
+import com.googlecode.lanterna.SGR;
+import com.googlecode.lanterna.TerminalPosition;
+import com.googlecode.lanterna.TextColor;
+import com.googlecode.lanterna.graphics.TextGraphics;
+
+public class Monster extends Element {
+
+    // Class methods
+    Monster(Position position) {
+
+        super(position);
+    }
+    public Position moveUp() {
+        return new Position(position.getX(), position.getY() - 1);
+    }
+    public Position moveRight() {
+        return new Position(position.getX() + 1, position.getY());
+    }
+    public Position moveDown() {
+        return new Position(position.getX(), position.getY() + 1);
+    }
+    public Position moveLeft() {
+        return new Position(position.getX() - 1, position.getY());
+    }
+    public void draw(TextGraphics graphics) {
+
+        graphics.setForegroundColor(TextColor.Factory.fromString("#00FF00"));
+        graphics.enableModifiers(SGR.BOLD);
+        graphics.putString(new TerminalPosition(position.getX(),position.getY()), "S");
+    }
+}
